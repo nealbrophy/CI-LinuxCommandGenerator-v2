d5374c9c707e0cd642c7cf64c0da97fa1c8e053e
Neal Brophy <nbrophy@pm.me>
2020-04-26 21:05:31 +0100

    Create distros and add_commands templates and routes
    start implementing create functionality

--- app.py
+++ app.py
@@ -11,12 +11,22 @@ app.config["MONGO_DBNAME"] = 'linuxCmdGen'
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 mongo = PyMongo(app)
-
+form_info ={}
 @app.route('/')
 def get_distros():
-    # _distros = mongo.db.distros.find()
-    # distro_list = [dist for dist in _distros]
-    return render_template('distros.html', distros=mongo.db.distros.find())
+    return render_template('distros.html', distros=mongo.db.distros.find(), form=form_info)
+
+@app.route('/add')
+def add_command():
+  return render_template('add_commands.html', distros=mongo.db.distros.find())
+
+@app.route('/insert_command', methods=['POST'])
+def insert_command():
+  form_info = request.form.to_dict()
+  # distros = mongo.db.distros
+  # distros.insert_one(request.form.to_dict())
+  return redirect(url_for('get_distros', form=form_info))
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
