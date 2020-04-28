84638bd3bf9fc53d3da9607b1992dbb569ca0a17
Neal Brophy <nbrophy@pm.me>
2020-04-28 18:43:53 +0100

    Implement basic create & update ops and populate README

--- app.py
+++ app.py
@@ -2,30 +2,40 @@ import os
 from os import path
 if path.exists("env.py"):
   import env 
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, redirect, request, url_for, abort
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+import pdb
 
 app = Flask(__name__)
+
 app.config["MONGO_DBNAME"] = 'linuxCmdGen'
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
+import find
+import insert
+
 mongo = PyMongo(app)
-form_info ={}
+
+# HOME VIEW
 @app.route('/')
 def get_distros():
-    return render_template('distros.html', distros=mongo.db.distros.find(), form=form_info)
+    return render_template('distros.html', 
+      distros=mongo.db.distros.find(), 
+      commands=mongo.db.commands.find())
 
+# ADD COMMANDS VIEW
 @app.route('/add')
 def add_command():
-  return render_template('add_commands.html', distros=mongo.db.distros.find())
+    return render_template('add_command.html', distros=mongo.db.distros.find(), commands=mongo.db.commands.find())
 
-@app.route('/insert_command', methods=['POST'])
-def insert_command():
-  form_info = request.form.to_dict()
-  # distros = mongo.db.distros
-  # distros.insert_one(request.form.to_dict())
-  return redirect(url_for('get_distros', form=form_info))
+
+
+# FIND COMMANDS VIEW
+@app.route('/find', methods=['GET']) 
+def find():
+  return render_template('find_command.html', distros=mongo.db.distros.find(), commands=mongo.db.commands.find())
+
 
 if __name__ == '__main__':
     app.run(debug=True)
