569bbc420a469fc5f0b0c32dca8451b423db2bb0
Neal Brophy <nbrophy@pm.me>
2020-04-29 17:48:16 +0100

    Move all views back to app.py, fix add view & implement edit/update view
    Consolidated app.routes back into single .py.
    Fix update view to include validation for pre-existing cmds.
    Implement basic edit & update view.

--- app.py
+++ app.py
@@ -12,9 +12,6 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'linuxCmdGen'
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
-import find
-import insert
-
 mongo = PyMongo(app)
 
 # HOME VIEW
@@ -29,13 +26,59 @@ def get_distros():
 def add_command():
     return render_template('add_command.html', distros=mongo.db.distros.find(), commands=mongo.db.commands.find())
 
+# INSERT COMMAND ACTION FROM ADD COMMAND VIEW
+@app.route('/insert_command', methods=['POST'])
+def insert_command():
+    # pdb.set_trace()
+    find_app = request.form.get('app_name')
+    find_distro = request.form.get('app_distro')
+    existing_cmds = mongo.db.commands.find()
+    for commands in existing_cmds:
+      if commands['app_name'] == find_app and commands['app_distro'] == find_distro:
+        return render_template('find_command.html', req_type='insert_fail',
+          results=mongo.db.commands.find({'app_name': {'$regex': find_app, '$options': 'ix'}, 'app_distro': find_distro}),
+          distros=mongo.db.distros.find(), commands=mongo.db.commands.find())
+      else:
+        commands = mongo.db.commands
+        commands.insert_one(request.form.to_dict())
+        return render_template('distros.html', distros=mongo.db.distros.find(), commands=mongo.db.commands.find())
+                
 
 
 # FIND COMMANDS VIEW
-@app.route('/find', methods=['GET']) 
-def find():
-  return render_template('find_command.html', distros=mongo.db.distros.find(), commands=mongo.db.commands.find())
+@app.route('/find_command', methods=['GET','POST']) 
+def find_command():
+  if request.method == 'GET':
+    return render_template('find_command.html', req_type='find', distros=mongo.db.distros.find())
+  else:
+    find_app = request.form.get('app_name')
+    find_distro = request.form.get('app_distro')
+    return render_template('find_command.html', req_type='find', 
+      distros=mongo.db.distros.find(),
+      results=mongo.db.commands.find({'app_name': {'$regex': find_app, '$options': 'ix'}, 'app_distro': find_distro}))
 
+
+# UPDATE COMMANDS VIEW
+@app.route('/edit/<command_id>')
+def edit_command(command_id):
+  cmd_to_update = mongo.db.commands.find_one({'_id': ObjectId(command_id)})
+  distros = mongo.db.distros.find()
+  return render_template('edit_command.html', cmd_to_update=cmd_to_update, distros=distros)
+
+@app.route('/update/<command_id>', methods=['POST'])
+def update_command(command_id):
+  commands = mongo.db.commands
+  commands.update({'_id': ObjectId(command_id)},
+  {
+    'app_name': request.form.get('app_name'),
+    'app_distro': request.form.get('app_distro'),
+    'app_instruction': request.form.get('app_instruction'),
+    'app_command': request.form.get('app_command')
+  })
+  # this_command = commands.find({'_id': ObjectId(command_id)})
+  return render_template('find_command.html', req_type='update',
+          results=mongo.db.commands.find({'_id': ObjectId(command_id)}),
+          distros=mongo.db.distros.find(), commands=mongo.db.commands.find())
 
 if __name__ == '__main__':
     app.run(debug=True)
