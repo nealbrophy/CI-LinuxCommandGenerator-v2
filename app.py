b2d34934d89dc7e28e842b18c48b28c896e7a598
Neal Brophy <nbrophy@pm.me>
2020-04-29 20:40:10 +0100

    Implement basic delete functionality
    Add Delete to complete CRUD. Start fleshing out app.route for confirming deletion.

--- app.py
+++ app.py
@@ -42,10 +42,8 @@ def insert_command():
         commands = mongo.db.commands
         commands.insert_one(request.form.to_dict())
         return render_template('distros.html', distros=mongo.db.distros.find(), commands=mongo.db.commands.find())
-                
 
-
-# FIND COMMANDS VIEW
+# FIND COMMAND VIEW
 @app.route('/find_command', methods=['GET','POST']) 
 def find_command():
   if request.method == 'GET':
@@ -57,14 +55,14 @@ def find_command():
       distros=mongo.db.distros.find(),
       results=mongo.db.commands.find({'app_name': {'$regex': find_app, '$options': 'ix'}, 'app_distro': find_distro}))
 
-
-# UPDATE COMMANDS VIEW
+# EDIT COMMAND VIEW
 @app.route('/edit/<command_id>')
 def edit_command(command_id):
   cmd_to_update = mongo.db.commands.find_one({'_id': ObjectId(command_id)})
   distros = mongo.db.distros.find()
   return render_template('edit_command.html', cmd_to_update=cmd_to_update, distros=distros)
 
+# UPDATE COMMAND ACTION FROM EDIT VIEW
 @app.route('/update/<command_id>', methods=['POST'])
 def update_command(command_id):
   commands = mongo.db.commands
@@ -75,11 +73,30 @@ def update_command(command_id):
     'app_instruction': request.form.get('app_instruction'),
     'app_command': request.form.get('app_command')
   })
-  # this_command = commands.find({'_id': ObjectId(command_id)})
   return render_template('find_command.html', req_type='update',
           results=mongo.db.commands.find({'_id': ObjectId(command_id)}),
           distros=mongo.db.distros.find(), commands=mongo.db.commands.find())
 
+@app.route('/confirm_delete/<command_id>')
+def confirm_delete(command_id):
+  return """
+  <h2>Confirm Deletion</h2>
+  <p>Are you sure you want to delete this command?</p>
+  <button>Confirm</button>
+  <button>Cancel</button>
+  """
+
+# DELETE COMMAND VIEW
+@app.route('/delete/<command_id>')
+def delete_command(command_id):
+  mongo.db.commands.remove({'_id': ObjectId(command_id)})
+  return render_template('find_command.html', req_type='find', distros=mongo.db.distros.find())
+
+
+
+
+
+# RUN Flask app
 if __name__ == '__main__':
     app.run(debug=True)
     
